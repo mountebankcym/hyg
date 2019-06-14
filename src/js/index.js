743346d6965a29720a5c73daa44117b49de7e634
mountebankcym <mountebank_limao@163.com>
2019-06-14 15:25:56 +0800

    banner

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,30 @@
 require(["config"], () => {
-	require(["template","url","footer", "header"], (template,url) => {
+	require(["template","url","swiper","footer", "header"], (template,url,Swiper) => {
 		class Index {
 			constructor() {
-				//this.banner();
+				this.banner();
 				this.a();
 				this.show();
 			}
 
-// 			banner() {
-// 				var mySwiper = new Swiper('.swiper-container', {
-// 					autoplay: true,
-// 					loop: true, // 循环模式选项
-// 					// 如果需要分页器
-// 					pagination: {
-// 						el: '.swiper-pagination',
-// 						clickable: true
-// 					},
-// 
-// 					// 如果需要前进后退按钮
-// 					navigation: {
-// 						nextEl: '.swiper-button-next',
-// 						prevEl: '.swiper-button-prev'
-// 					}
-// 
-// 				})
-// 			}
+			banner() {
+				var mySwiper = new Swiper('.swiper-container', {
+					autoplay: true,
+					loop: true, // 循环模式选项
+					// 如果需要分页器
+					pagination: {
+						el: '.swiper-pagination',
+						clickable: true
+					},
+
+					// 如果需要前进后退按钮
+					navigation: {
+						nextEl: '.swiper-button-next',
+						prevEl: '.swiper-button-prev'
+					}
+
+				})
+			}
 
 			a() {
 				console.log(1111);
